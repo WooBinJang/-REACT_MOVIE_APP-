9405c75f75c7d253d843275db06ffbead0290704
Jang Oo Bin <jwb449730@naver.com>
2021-12-14 18:41:49 +0900

    cleanup function

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,26 @@
-import Button from "./button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi");
+    return () => console.log("bye");
+    // 컴포넌트가 파괴될 때도 함수를 실행하고 싶으면
+    // useEffect 함수가 새로운 함수를 return
+  }, []);
+  return <h1>Hello</h1>;
+}
+// dependency가 비어있으면 자동으로 컴포넌트가 파괴될 때 cleanup함수가 실행되는데
+// 그 과정이 리렌더링으로 useEffect함수가 실행되고 클린업하면서
+// 이전에 있던 이펙트인 console.log(“hi")가 삭제되고
+// 새로운 이펙트 함수인 return 함수가 실행
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("리랜더링");
-  const iRunOnlyOnce = () => {
-    console.log("최초의 한번만 랜더링");
-  };
-  useEffect(iRunOnlyOnce, []); //useEffect((함수),[값]) : API를 딱 한번만 호출하고 그 뒤로는 다시는 호출하기 싫은 경우 값이 변경이 랜더링
-  useEffect(() => {
-    console.log("키워드 입력시 랜더링");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("카운터 시 랜더링");
-  }, [counter]);
-  useEffect(() => {
-    console.log("카운터 또는 키워드 입력 시 랜더링 ");
-  }, [counter, keyword]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      ></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button text={"continue"} />
-      <button onClick={onClick}>Click Me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
