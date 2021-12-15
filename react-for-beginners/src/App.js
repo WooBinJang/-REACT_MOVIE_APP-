1b890030e4333603bf18f487b4ac3749fc26cbde
Jang Oo Bin <jwb449730@naver.com>
2021-12-15 18:41:41 +0900

    React_Router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,70 +1,19 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [moneyChk, setMoneyChk] = useState(false);
-  const [money, setMoney] = useState(0);
-  const [coins, setCoins] = useState([]);
-  const [selectedCoin, setSelectedCoin] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  const onChange = (event) => {
-    setMoney(event.target.value);
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setMoneyChk(true);
-  };
-
-  const handleChangeSelect = (event) => {
-    const coinInfo = event.target.value.split(" ");
-
-    setSelectedCoin([
-      coinInfo[0] + " " + coinInfo[1],
-      coinInfo[3].substring(1, coinInfo[3].length),
-    ]);
-  };
   return (
-    <div>
-      <h1> {loading ? "The Coins !" : `The Coins : ${coins.length}`}</h1>
-      {moneyChk ? (
-        <h2>i have {money} $</h2>
-      ) : (
-        <form onSubmit={onSubmit}>
-          <input
-            type="number"
-            value={money}
-            onChange={onChange}
-            placeholder="How much money do you have?"
-          ></input>
-          <button>입력</button>
-        </form>
-      )}
-
-      {loading ? (
-        <strong>loading</strong>
-      ) : (
-        <select onChange={handleChangeSelect} defaultValue="Choose the coin">
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
-      {isNaN(selectedCoin) ? (
-        <h2>You can buy {Math.floor(money / selectedCoin[1])} </h2>
-      ) : null}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
