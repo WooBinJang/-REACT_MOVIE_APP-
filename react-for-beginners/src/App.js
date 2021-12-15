3b7d7a578602e193e92fb2464d153b4a8dbb60b1
Jang Oo Bin <jwb449730@naver.com>
2021-12-15 17:14:47 +0900

    coin_Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,41 +1,69 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState(""); //state는 직접적으로 수정 불가능 함수를 가져와서 수정
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [moneyChk, setMoneyChk] = useState(false);
+  const [money, setMoney] = useState(0);
+  const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
+    setMoneyChk(true);
   };
-  console.log(toDos);
+
+  const handleChangeSelect = (event) => {
+    const coinInfo = event.target.value.split(" ");
+
+    setSelectedCoin([
+      coinInfo[0] + " " + coinInfo[1],
+      coinInfo[3].substring(1, coinInfo[3].length),
+    ]);
+  };
   return (
     <div>
-      <h1>My To Dos : {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {
-          //map의 첫 번째 argument는 값이고 두번째는 index
-          toDos.map((toDo, index) => (
-            <li key={index}>{toDo}</li>
-          ))
-          //리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에 key를 넣어 고유하게 만들어줘야함
-        }
-      </ul>
+      <h1> {loading ? "The Coins !" : `The Coins : ${coins.length}`}</h1>
+      {moneyChk ? (
+        <h2>i have {money} $</h2>
+      ) : (
+        <form onSubmit={onSubmit}>
+          <input
+            type="number"
+            value={money}
+            onChange={onChange}
+            placeholder="How much money do you have?"
+          ></input>
+          <button>입력</button>
+        </form>
+      )}
+
+      {loading ? (
+        <strong>loading</strong>
+      ) : (
+        <select onChange={handleChangeSelect} defaultValue="Choose the coin">
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
+      {isNaN(selectedCoin) ? (
+        <h2>You can buy {Math.floor(money / selectedCoin[1])} </h2>
+      ) : null}
     </div>
   );
 }
