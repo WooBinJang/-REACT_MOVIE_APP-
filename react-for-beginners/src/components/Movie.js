30938693714b9163ccc325ff70c500bcfcea0b31
Jang Oo Bin <jwb449730@naver.com>
2021-12-18 19:50:23 +0900

    css

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,23 +1,31 @@
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-function Movie({ id, coverImg, title, summary, genres }) {
+import styles from "./Movie.module.css";
+
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres && genres.map((g) => <li key={g}>{g}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
 
-Movie.propType = {
-  coverImg: propTypes.string.isRequired,
-  title: propTypes.string.isRequired,
-  summary: propTypes.string.isRequired,
-  genres: propTypes.arrayOf(propTypes.string).isRequired,
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
+import styled from "styled-components";
 
 function Detail() {
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState([]);
   const { id } = useParams();
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
@@ -17,17 +20,41 @@ function Detail() {
     getMovie();
   }, []);
 
+  const Container = styled.div`
+    background-image: url(${movie.background_image_original});
+    background-size: cover;
+    height: 100vh;
+  `;
   console.log(movie);
   return (
-    <div>
-      {loading ? <strong>Loading~~</strong> : null}
-      <img src={movie.medium_cover_image} alt={movie.title}></img>
-      <h1>{movie.title_long}</h1>
-      <p>{`Runtime : ${movie.runtime} min`}</p>
-      <p>{`language : ${movie.language}`}</p>
-      <p>{`Description : ${movie.description_full}`}</p>
-      <p>{`Rating : ${movie.rating}`}</p>
-    </div>
+    <Container>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div>
+          <img
+            className={styles.movie_img}
+            src={movie.medium_cover_image}
+            alt={movie.title}
+          ></img>
+          <div
+            className={styles.movie_info}
+          >{`Title : ${movie.title_long} `}</div>
+          <div
+            className={styles.movie_info}
+          >{`Runtime : ${movie.runtime} min`}</div>
+          <div
+            className={styles.movie_info}
+          >{`language : ${movie.language}`}</div>
+          <div
+            className={styles.movie_info}
+          >{`Description : ${movie.description_full}`}</div>
+          <div className={styles.movie_info}>{`Rating : ${movie.rating}`}</div>
+        </div>
+      )}
+    </Container>
   );
 }
 
